3ab6139758e00fc908d46685d7bcf4be401de51d
Paul Horstmann <paulluishorstmann@gmail.com>
2021-03-19 21:08:49 +0100

    Push to Pi

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const bodyParser = require("body-parser");
 const fs = require('fs');
 
 const app = express();
-const port = 3000;
+const port = 80;
 
 const { response } = require('express');
 const { spawn } = require('child_process');
